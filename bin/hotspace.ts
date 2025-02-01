4e1cd4afef4530a50d7f8c1950490e19d6914647
Martin Petrov <martin_petrov@me.com>
2025-02-01 07:27:56 +0200

    remove aws-region from config

--- bin/hotspace.ts
+++ bin/hotspace.ts
@@ -8,13 +8,11 @@ const app = new cdk.App();
 new ProdStage(app, "Prod", {
   env: {
     account: process.env.AWS_PROD_ACCOUNT,
-    region: process.env.AWS_REGION,
   },
 });
 
 new DevStage(app, "Dev", {
   env: {
     account: process.env.AWS_DEV_ACCOUNT,
-    region: process.env.AWS_REGION,
   },
 });
