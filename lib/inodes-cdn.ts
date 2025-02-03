638871cdb06dfb4be02f6a6bc17db30f22ef8f18
Martin Petrov <martin_petrov@me.com>
2025-02-03 10:14:59 +0200

    add cloudfront signer

--- lib/inodes-cdn.ts
+++ lib/inodes-cdn.ts
@@ -11,11 +11,21 @@ export class InodesCdn extends Construct {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id);
 
+    const bucket = props.inodesBucket.bucket;
+    const encodedKey = process.env.CLOUDFRONT_SIGNER_PUBKEY;
+
+    if (!encodedKey) throw new Error("Missing CLOUDFRONT_SIGNER_PUBKEY");
+
+    const pubKey = new cloudfront.PublicKey(this, "PubKey", { encodedKey });
+
+    const keyGroup = new cloudfront.KeyGroup(this, "KeyGroup", {
+      items: [pubKey],
+    });
+
     new cloudfront.Distribution(this, "Distribution", {
       defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(
-          props.inodesBucket.bucket
-        ),
+        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
+        trustedKeyGroups: [keyGroup],
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
         cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
